cfa516740cafc85b7f62fd4f234065387618fee7
Cathy Qin <qin.xuj@northeastern.edu>
2024-04-15 23:16:29 -0700

    add form submit banner

--- spring-boot-bootcamp-resources-main/02. model-view-controller/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/Constants.java
+++ spring-boot-bootcamp-resources-main/02. model-view-controller/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/Constants.java
@@ -3,4 +3,6 @@ package com.ltp.globalsuperstore;
 public class Constants {
     public static final int NOT_FOUND = -10000;
     public static final String[] CATEGORIES = new String[] {"Furniture", "Office Supplies", "Technology"};
+    public static final String SUCCESS_STATUS = "success";
+    public static final String FAILURE_STATUS = "failure";
 }
--- spring-boot-bootcamp-resources-main/02. model-view-controller/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
+++ spring-boot-bootcamp-resources-main/02. model-view-controller/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
@@ -1,12 +1,15 @@
 package com.ltp.globalsuperstore;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class StoreController {
@@ -18,6 +21,20 @@ public class StoreController {
     model.addAttribute("categories", Constants.CATEGORIES);
     return "form";
   }
+  @PostMapping("/submitItem")
+  public String handleSubmit(Item item, RedirectAttributes redirectAttributes){
+    int idx = getIndexFromId(item.getId());
+    String status = Constants.SUCCESS_STATUS;
+    if(idx == Constants.NOT_FOUND) items.add(item);
+    else if(within5Days(item.getDate(), items.get(idx).getDate())){
+      items.set(idx, item);
+    } else{
+      status = Constants.FAILURE_STATUS;
+    }
+    // save flash attribute
+    redirectAttributes.addFlashAttribute("status", status);
+    return "redirect:/inventory";
+  }
 
   @GetMapping("/inventory")
   public String getInventory(Model model){
@@ -25,18 +42,15 @@ public class StoreController {
     return "inventory";
   }
 
-  @PostMapping("/submitItem")
-  public String handleSubmit(Item item){
-    int idx = getIndexFromId(item.getId());
-    if(idx == Constants.NOT_FOUND) items.add(item);
-    else items.set(idx, item);
-
-    return "redirect:/inventory";
-  }
   public int getIndexFromId(String id) {
     for (int i = 0; i < items.size(); i++) {
       if(items.get(i).getId().equals(id)) return i;
     }
     return Constants.NOT_FOUND;
+  }
+
+  public boolean within5Days(Date newDate, Date oldDate) {
+     long diff = Math.abs(newDate.getTime() - oldDate.getTime());
+     return (int)(TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
   }
 }

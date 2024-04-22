64ac4299b38cece0ce79c923fb8d13a2a76df409
Cathy Qin <qin.xuj@northeastern.edu>
2024-04-21 23:30:49 -0700

    add validation annotation

--- 03. field-validation/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/Item.java
+++ 03. field-validation/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/Item.java
@@ -3,13 +3,23 @@ package com.ltp.globalsuperstore;
 import java.util.Date;
 import java.util.UUID;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
 import org.springframework.format.annotation.DateTimeFormat;
 
 public class Item {
+    @NotBlank(message = "Please select a category")
     private String category;
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+    @Min(value = 0, message = "Price cannot be negative")
     private Double price;
+    @Min(value = 0, message = "Discount cannot be negative")
     private Double discount;
+
+    @Past(message = "Date must be of the past")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date;
     private String id;

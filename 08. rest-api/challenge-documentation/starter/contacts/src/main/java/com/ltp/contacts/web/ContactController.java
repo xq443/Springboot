a38ca7cb6da243f4a243f41375e39424a494ad7f
Cathy Qin <qin.xuj@northeastern.edu>
2024-05-05 01:35:30 -0700

    add: OPEN API documentation

--- 08. rest-api/challenge-documentation/starter/contacts/src/main/java/com/ltp/contacts/web/ContactController.java
+++ 08. rest-api/challenge-documentation/starter/contacts/src/main/java/com/ltp/contacts/web/ContactController.java
@@ -1,11 +1,20 @@
 package com.ltp.contacts.web;
 
+import com.ltp.contacts.exception.ErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,28 +25,42 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.service.ContactService;
 
+@Tag(name = "Controller", description = "Create and retrieve contacts")
 @RestController
 public class ContactController {
     
     @Autowired
     private ContactService contactService;
 
-    @GetMapping(value = "/contact/all")
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of contacts",
+        content = @Content(array = @ArraySchema(schema = @Schema(implementation = Contact.class))))
+    @Operation(summary = "Retrieves contacts", description = "Provides a list of all contacts")
+    @GetMapping(value = "/contact/all", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Contact>> getContacts() {
         List<Contact> contacts = contactService.getContacts();
         return new ResponseEntity<>(contacts, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/contact/{id}")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "404", description = "Contact doesn't exist", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(responseCode = "200", description = "Successful retrieval of contact", content = @Content(schema = @Schema(implementation = Contact.class))),
+    })
+    @Operation(summary = "Get contact by Id", description = "Returns a contact based on an ID")
+    @GetMapping(value = "/contact/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Contact> getContact(@PathVariable String id) {
         Contact contact = contactService.getContactById(id);
         return new ResponseEntity<>(contact, HttpStatus.OK);
     }
 
-    @PostMapping(value = "/contact")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Successful creation of contact"),
+        @ApiResponse(responseCode = "400", description = "Bad request: unsuccessful submission",
+            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @Operation(summary = "Create Contact", description = "Creates a contact from the provided payload")
+    @PostMapping(value = "/contact", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Contact> createContact(@Valid @RequestBody Contact contact) {
         contactService.saveContact(contact);
         return new ResponseEntity<>(contact, HttpStatus.CREATED);
     }
-
 }

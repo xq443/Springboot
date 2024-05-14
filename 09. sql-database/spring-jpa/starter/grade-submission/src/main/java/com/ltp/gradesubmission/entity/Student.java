aed3d01f6984307366d52b7e1970990c3a127764
Cathy Qin <qin.xuj@northeastern.edu>
2024-05-13 22:53:19 -0700

    add: create table JPA

--- 09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Student.java
+++ 09. sql-database/spring-jpa/starter/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Student.java
@@ -1,11 +1,27 @@
 package com.ltp.gradesubmission.entity;
 
 import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+
+@Entity
+@Table(name = "student")
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
 
     public Long getId() {

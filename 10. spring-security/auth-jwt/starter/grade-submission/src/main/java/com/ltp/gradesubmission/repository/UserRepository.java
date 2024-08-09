30234124d5aff3e7c8e22da834604fd81acf8a23
Cathy Qin <qin.xuj@northeastern.edu>
2024-08-09 01:41:29 -0700

    finalized

--- 10. spring-security/auth-jwt/starter/grade-submission/src/main/java/com/ltp/gradesubmission/repository/UserRepository.java
+++ 10. spring-security/auth-jwt/starter/grade-submission/src/main/java/com/ltp/gradesubmission/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.ltp.gradesubmission.entity.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-	Optional<User> findByUsername(String username);
+	Optional<User> findByUsername(String username); // optional
 }
